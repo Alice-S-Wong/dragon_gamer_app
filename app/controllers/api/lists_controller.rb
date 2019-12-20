d959bd3d63def2c12d0be0300f74aefaf89b9820
Alice <alicexenawong@gmail.com>
2019-12-19 19:26:27 -0800

    Add a few validations for list creation.

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -6,8 +6,11 @@ class Api::ListsController < ApplicationController
         list_type: params[:list_type],
         visibility: params[:visibility],
       )
-      @list.save
-      render "show.json.jb"
+      if @list.save
+        render "show.json.jb"
+      else
+        render json: {errors: @list.errors.full_messages}, status: :unprocessable_entity
+      end
     else
       render json: {message: "Must be logged in to create a list"}, status: :unauthorized
     end
