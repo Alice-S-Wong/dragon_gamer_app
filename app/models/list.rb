2db8df82ce2832b9a6837ee1a5eb9db9a0bdd3bd
Alice <alicexenawong@gmail.com>
2020-01-02 18:07:57 -0800

    Update show view for lists to show list item ids for the frontend.

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,8 +2,8 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :list_items
   has_many :games, through: :list_items
-  validates :list_type, inclusion: { in: %w(favorites wishlist played),
-    message: "%{value} is not a valid list type" }
-  validates :visibility, inclusion: { in: %w(public friends private),
-    message: "%{value} is not a valid visibility type" }
+  # validates :list_type, inclusion: { in: %w(favorites wishlist played),
+  #   message: "%{value} is not a valid list type" }
+  # validates :visibility, inclusion: { in: %w(public friends private),
+  #   message: "%{value} is not a valid visibility type" }
 end
